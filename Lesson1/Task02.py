c05d00c4e0c1c4127d76429e8db9285839261ce2
Nostromo-04 <89577328+Nostromo-04@users.noreply.github.com>
2021-10-14 11:58:41 +0500

    Update Task02.py

--- Lesson1/Task02.py
+++ Lesson1/Task02.py
@@ -7,6 +7,6 @@ if times < 86400:
     hours = times // 3600
     minutes = times % 3600 // 60
     seconds = times % 3600 % 60
-    print(f"{hours}:{minutes}:{seconds}")
+    print(f"{hours:>02}:{minutes:>02}:{seconds:>02}")
 else:
     print("ОШИБКА: Максимальное число 86400")
